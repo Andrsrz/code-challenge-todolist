75f04b9a9eb9abf07a5c57d447a990822d676baf
Andres Ruiz <andrsruiz@gmail.com>
2021-07-18 14:33:48 -0500

    Proper Styles

--- todoapp/src/App.vue
+++ todoapp/src/App.vue
@@ -1,33 +1,20 @@
 <template>
   <div id="app">
     <Home/>
-		<ToDoForm/>
-		<ToDoListForm/>
   </div>
 </template>
 
 <script>
 import Home from './views/Home.vue'
-import ToDoForm from './components/ToDoForm.vue'
-import ToDoListForm from './components/ToDoListForm.vue'
 
 export default {
   name: 'App',
   components: {
     Home,
-		ToDoForm,
-		ToDoListForm
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang='css'>
+@import 'assets/css/app.css';
 </style>
