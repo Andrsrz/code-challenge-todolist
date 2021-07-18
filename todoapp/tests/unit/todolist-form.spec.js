1b2674665043368be075e632699a7aaae7faa92e
Andres Ruiz <andrsruiz@gmail.com>
2021-07-18 13:10:40 -0500

    Test for default data

--- todoapp/tests/unit/todolist-form.spec.js
+++ todoapp/tests/unit/todolist-form.spec.js
@@ -7,9 +7,15 @@ localVue.use(Buefy)
 
 const wrapper = shallowMount(ToDoListForm, { localVue })
 
-descirbe('ToDo List Form', () => {
+describe('ToDo List Form', () => {
 	it('should set the correct default data', () => {
 		const data = wrapper.vm.$data
 		expect(typeof wrapper.vm.$data).toBe('object')
+		expect(data.modalTitle).toMatch('New ToDo List')
+		expect(data.labelTitle).toMatch('Title')
+		expect(data.labelDescription).toMatch('Description')
+		expect(data.validationMessage).toMatch('Fill out before submit')
+		expect(data.inputTitle).toMatch('')
+		expect(data.inputDescription).toMatch('')
 	})
 })
