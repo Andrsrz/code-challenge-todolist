a8b1b58468d99591259904cacf6dbf188be458aa
Andres Ruiz <andrsruiz@gmail.com>
2021-07-19 12:33:47 -0500

    Can retreive all todo lists

--- todoapp/src/components/ToDoListsList.vue
+++ todoapp/src/components/ToDoListsList.vue
@@ -1,10 +1,34 @@
 <template>
 	<div id='todolists-list'>
+		<ToDoList v-for='(todoList, index) in todoLists' :key='index' :todoList='todoList'/>
 	</div>
 </template>
 
 <script lang='js'>
+import ToDoList from '../components/ToDoList.vue'
+
 export default{
-	name: 'ToDoListsList'
+	name: 'ToDoListsList',
+	components: { ToDoList },
+	data(){
+		return{
+			todoLists: [],
+			isData: false
+		}
+	},
+	methods: {
+		init(){ this.getAll() },
+		getAll(){
+			fetch('http://localhost:3000/api/todolist/all', { method: 'GET' })
+				.then(response => response.json())
+				.then(data => {
+					if(data.length !== 0){
+						this.isData = true
+						this.todoLists = data
+					}
+				}).catch(error => { console.error(error) })
+		}
+	},
+	mounted(){ this.init() }
 }
 </script>
