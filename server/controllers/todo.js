cbebfacdea8a8d8a8d7e2138780cc873a7f43eae
Andres Ruiz <andrsruiz@gmail.com>
2021-07-19 02:01:48 -0500

    Add Controllers

---
+++ server/controllers/todo.js
@@ -0,0 +1,61 @@
+const ToDo = require('../models/todo.js')
+
+exports.Create = (req, res) => {
+	const myToDo = new ToDo({
+		title: req.body.title,
+		description: req.body.description,
+		dueDate: req.body.dueDate,
+		priority: req.body.priority
+	}).save(err => {
+		if(err)
+			return res.status(406).json(err)
+
+		res.status(201).json({ message: 'ToDo Created' })
+	})
+}
+
+exports.GetAll = (req, res) => {
+	ToDo.find({ parent: req.query.parent })
+			.populate('title')
+			.sort(['dueDate', 'ascending'])
+			.exec((err, ToDos) => {
+				if(err)
+					return res.status(406).json(err)
+
+				return res.status(200).json(ToDos)
+			})
+}
+
+exports.GetById = (req, res) => {
+	ToDo.findById(req.query.id, (err, todo) => {
+		if(err)
+			return res.status(404).json(err)
+
+		return res.status(200).json(todo)
+	})
+}
+
+exports.Update = (req, res) => {
+	ToDo.findByIdAndUpdate(req.body.id, {
+		_id: req.body.id,
+		title: req.body.title,
+		description: req.body.description,
+		dueDate: req.body.dueDate,
+		priority: req.body.priority,
+		updatedOn: new Date
+	}, err => {
+		if(err)
+			return res.status(406).json(err)
+
+		return res.status(201).json({ message: 'ToDo Updated' })
+	})
+}
+
+exports.Delete = (req, res) => {
+	ToDo.findByIdAndDelete(req.body.id, err => {
+		if(err)
+			return re.status(406).json(err)
+
+		return res.status(200).json({ message: 'ToDo Deleted' })
+	})
+}
---
+++ server/controllers/todoList.js
@@ -0,0 +1,57 @@
+const ToDoList = require('../models/todoList.js')
+
+exports.Create = (req, res) => {
+	const myToDoList = new ToDoList({
+		title: req.body.title,
+		description: req.body.description
+	}).save(err => {
+		if(err)
+			return res.status(406).json(err)
+
+		res.status(201).json({ message: 'ToDo List Created' })
+	})
+}
+
+exports.GetAll = (req, res) => {
+	ToDoList.find()
+			.populate('title')
+			.sort(['createdOn', 'ascending'])
+			.exec((err, ToDoLists) => {
+				if(err)
+					return res.status(406).json(err)
+
+				return res.status(200).json(ToDoLists)
+			})
+}
+
+exports.GetById = (req, res) => {
+	ToDoList.findById(req.query.id, (err, todoList) => {
+		if(err)
+			return res.status(404).json(err)
+
+		return res.status(200).json(todoList)
+	})
+}
+
+exports.Update = (req, res) => {
+	ToDoList.findByIdAndUpdate(req.body.id, {
+		_id: req.body.id,
+		title: req.body.title,
+		description: req.body.description,
+		updatedOn: new Date
+	}, err => {
+		if(err)
+			return res.status(406).json(err)
+
+		return res.status(201).json({ message: 'ToDo List Updated' })
+	})
+}
+
+exports.Delete = (req, res) => {
+	ToDoList.findByIdAndDelete(req.body.id, err => {
+		if(err)
+			return re.status(406).json(err)
+
+		return res.status(200).json({ message: 'ToDo List Deleted' })
+	})
+}
