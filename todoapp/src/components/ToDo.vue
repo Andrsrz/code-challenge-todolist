ba391d7efe0d442402233c7eb35722fbaaeb6a76
Andres Ruiz <andrsruiz@gmail.com>
2021-07-19 23:36:12 -0500

    Set Style to ToDo

--- todoapp/src/components/ToDo.vue
+++ todoapp/src/components/ToDo.vue
@@ -1,12 +1,70 @@
 <template>
-	<div class='todo'>
-		{{ todo }}
+	<div id='todo' :class='todo.priority'>
+		<b-checkbox v-model='todo.done' type="is-success"></b-checkbox>
+		<span id='title-container'>
+			<h1>{{ todo.title }}</h1>
+			<p>Due Date: {{ todo.dueDate }}</p>
+		</span>
+		<h3>{{ todo.description }}</h3>
 	</div>
 </template>
 
 <script lang='js'>
+import moment from 'moment'
+
 export default{
 	name: 'ToDo',
-	props: { todo: Object }
+	props: { todo: Object },
+	methods: {
+		init(){ this.parseDate() },
+		parseDate(){
+			this.todo.dueDate = moment(this.todo.dueDate).format('MM/DD/YYYY')
+		}
+	},
+	created(){ this.init() }
 }
 </script>
+
+<style lang='css' scoped>
+#todo {
+	width: 80%;
+	display: flex;
+	flex-flow: column nowrap;
+	border-radius: 5px;
+}
+
+.High {
+	border: 1px solid red;
+}
+
+.Neutral {
+	border: 1px solid orange;
+}
+
+.Low {
+	border: 1px solid yellow;
+}
+
+.High:hover {
+	border: 3px solid red;
+}
+
+.Neutral:hover {
+	border: 3px solid orange;
+}
+
+.Low:hover {
+	border: 3px solid yellow;
+}
+
+#todo > #title-container {
+	display: flex;
+	flex-flow: row nowrap;
+	justify-content: space-between;
+}
+
+* {
+	margin: 5px;
+	padding: 2.5px;
+}
+</style>
