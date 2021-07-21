61cd1ccbb192fee0d4a8429ca8c8c19a1eb2cdaa
Andres Ruiz <andrsruiz@gmail.com>
2021-07-21 00:19:12 -0500

    Remove colons

--- todoapp/tests/unit/page-not-found.spec.js
+++ todoapp/tests/unit/page-not-found.spec.js
@@ -1,22 +1,22 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils';
-import router from '@/router';
-import Buefy from 'buefy';
-import PageNotFound from '@/views/PageNotFound.vue';
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import router from '@/router'
+import Buefy from 'buefy'
+import PageNotFound from '@/views/PageNotFound.vue'
 
-const localVue = createLocalVue();
-localVue.use(Buefy);
+const localVue = createLocalVue()
+localVue.use(Buefy)
 
-const wrapper = shallowMount(PageNotFound, { router, localVue });
+const wrapper = shallowMount(PageNotFound, { router, localVue })
 
 describe('PageNotFound Component', () => {
 	it('Renders a main div container', () => {
-		expect(wrapper.get('#page-not-found')).toBeTruthy();
-	});
+		expect(wrapper.get('#page-not-found')).toBeTruthy()
+	})
 
-	const data = wrapper.vm.$data;
+	const data = wrapper.vm.$data
 	it('Should set the default data', () => {
-		expect(typeof wrapper.vm.$data).toBe('object');
-		expect(data.title).toMatch('Page Not Found');
-		expect(data.labelButton).toMatch('Go Back');
-	});
-});
+		expect(typeof wrapper.vm.$data).toBe('object')
+		expect(data.title).toMatch('Page Not Found')
+		expect(data.labelButton).toMatch('Go Back')
+	})
+})
